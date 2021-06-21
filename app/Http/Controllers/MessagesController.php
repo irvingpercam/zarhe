59654c54586f073f897b44b2ae69844a772b3484
alainware <a01703171@itesm.mx>
2021-06-21 01:29:08 -0500

    Upload: "Translation files and mailing service configuration"

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -1,13 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Mail\MessageReceived;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Http\Request;
 
 class MessagesController extends Controller
 {
     public function store(Request $request)
     {
-        return $request;
+        $message = request()->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'subject' => 'required|max:100',
+            'content' => 'required|min:3'
+        ]);
+
+        Mail::to('irving6258@gmail.com')->queue(new MessageReceived($message));
+
+        return 'Mensaje enviado';
     }
 }
--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -4,14 +4,14 @@
     <h1>Contacto</h1>
     <form method="POST" action="{{ route('contacto') }}">
         @csrf
-        <input type="text" name="name" placeholder="Nombre...">
-        <br>
-        <input type="email" name="email" placeholder="Email...">
-        <br>
-        <input type="text" name="subject" placeholder="Asunto...">
-        <br>
-        <textarea name="content" cols="30" rows="10" placeholder="Mensaje..."></textarea>
-        <br>
+        <input type="text" name="name" placeholder="Nombre..." value="{{ old('name') }}">
+        <br>{!! $errors->first('name', '<small>:message</small><br>') !!}
+        <input type="email" name="email" placeholder="Email..." value="{{ old('email') }}">
+        <br>{!! $errors->first('email', '<small>:message</small><br>') !!}
+        <input type="text" name="subject" placeholder="Asunto..." value="{{ old('subject') }}">
+        <br>{!! $errors->first('subject', '<small>:message</small><br>') !!}
+        <textarea name="content" cols="30" rows="10" placeholder="Mensaje...">{{ old('content') }}</textarea>
+        <br>{!! $errors->first('content', '<small>:message</small><br>') !!}
         <button>Enviar</button>
     </form>
 @endsection
